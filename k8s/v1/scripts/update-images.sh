70da09f6854cd2275de52f7098f83d4d4a3699ff
dbortnichuk <dbortnichuk@gmail.com>
2021-09-14 12:37:02 +0300

    mysql completed

--- k8s/v1/scripts/update-images.sh
+++ k8s/v1/scripts/update-images.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -ex
 set -o pipefail
 
-sbt clean assembly
+#sbt clean assembly
 
 docker build --file ./docker/v1/DockerfileBackend -t dbortnichuk/backend_scala:latest .
 docker build --file ./docker/v1/DockerfileFrontend -t dbortnichuk/frontend_scala:latest .
+docker build --file ./k8s/v1/mysql/Dockerfile -t dbortnichuk/db-mysql-test:8.0.26 .
 
 docker login
 
 docker push dbortnichuk/backend_scala:latest
 docker push dbortnichuk/frontend_scala:latest
+docker push dbortnichuk/db-mysql-test:8.0.26
 
 # cleanup
 docker rmi $(docker images | grep "<none>"|awk '$2=="<none>" {print $3}') --force
